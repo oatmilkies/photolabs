d7de1ea84f238f0fb60c0c8114a1f51b817df3ac
Leilani <pquaby@gmail.com>
2024-05-20 13:02:32 -0400

    Change styling

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -6,11 +6,12 @@ import '../styles/PhotoFavButton.scss';
 function PhotoFavButton(props) {
   const [liked, setLike] = useState(false);
   const handleClick = () => {setLike(prevLiked => !prevLiked)};
-  const favIconClass = liked ? "photo-list__fav-icon-svg" : "photo-list__fav-icon";
+  //const favIconClass = liked ? "photo-list__fav-icon-svg" : "photo-list__fav-icon";
 
   return (
-    <div onClick={handleClick} className={favIconClass}>
-        <FavIcon />
+    <div onClick={handleClick}>
+        {liked && <div className="photo-list__fav-icon-svg" ><FavIcon /></div>}
+        {!liked && <div className="photo-list__fav-icon" ><FavIcon /></div>}
     </div>
   );
 }
