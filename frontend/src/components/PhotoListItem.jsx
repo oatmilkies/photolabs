1afd5eadfe78819d222db2c51d462b68d944350e
Leilani <pquaby@gmail.com>
2024-05-20 10:48:43 -0400

    Change styling in photolistitem

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -5,25 +5,17 @@ import "../styles/PhotoListItem.scss";
 
 const PhotoListItem = (props) => {
   return (
-    <ul>
-      <li className="photo-list__item" key={props.listItem.id}>
-        <div>
-          <img className="photo-list__image" src={props.listItem.imageSource}></img>
+    <li className="photo-list__item" key={props.listItem.id}>
+      <img className="photo-list__image" src={props.listItem.imageSource}></img>
+      <img className="photo-list__user-profile" src={props.listItem.profile}></img>
+      <div className="photo-list__user-detail">
+        <span className="photo-list__user-info">{props.listItem.username}</span>
+        <div className="photo-list__user-location">
+          <span>{props.listItem.location.city}</span>
+          <span>{props.listItem.location.country}</span>
         </div>
-        <span>
-          <img className="photo-list__user-profile" src={props.listItem.profile}></img>
-        </span>
-
-        <span className="photo-list__user-detail">
-          <span className="photo-list__user-info">{props.listItem.username}</span>
-          <div className="photo-list__user-location">
-            <span>{props.listItem.location.city}</span>
-            <span>{props.listItem.location.country}</span>
-          </div>
-        </span>
-
-      </li>
-    </ul>
+      </div>
+    </li>
   );
 };
 
