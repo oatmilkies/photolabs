e341f2b906f3a1746ee24f9d83c9ee7ed9e5498e
Leilani <pquaby@gmail.com>
2024-05-20 10:50:54 -0400

    Fix typo

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -8,7 +8,7 @@ const PhotoListItem = (props) => {
     <li className="photo-list__item" key={props.listItem.id}>
       <img className="photo-list__image" src={props.listItem.imageSource}></img>
       <img className="photo-list__user-profile" src={props.listItem.profile}></img>
-      <div className="photo-list__user-detail">
+      <div className="photo-list__user-details">
         <span className="photo-list__user-info">{props.listItem.username}</span>
         <div className="photo-list__user-location">
           <span>{props.listItem.location.city}</span>
