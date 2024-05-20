ba062b2f628457299b8a2cdc970d5a4a63d1b263
Leilani <pquaby@gmail.com>
2024-05-20 10:56:12 -0400

    Move profile img to inside user div

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -7,8 +7,9 @@ const PhotoListItem = (props) => {
   return (
     <li className="photo-list__item" key={props.listItem.id}>
       <img className="photo-list__image" src={props.listItem.imageSource}></img>
-      <img className="photo-list__user-profile" src={props.listItem.profile}></img>
+
       <div className="photo-list__user-details">
+      <img className="photo-list__user-profile" src={props.listItem.profile}></img>
         <div className="photo-list__user-info">
           <span>{props.listItem.username}</span>
           <div className="photo-list__user-location">
