2e235e0c47440233c0c6c224d53dee8895a60ccb
Leilani <pquaby@gmail.com>
2024-05-21 17:43:05 -0400

    Add nav bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
 
-import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
-import FavIcon from 'components/FavIcon';
 import PhotoList from 'components/PhotoList';
-import TopicList from 'components/TopicList';
+import TopNavigation from 'components/TopNavigationBar'
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   return (
     <div className="App">
-      <TopicList />
+      <TopNavigation />
       <PhotoList />
     </div>
 
