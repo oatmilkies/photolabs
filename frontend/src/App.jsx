f8df917d2bf84811287f50dbd6a06e73076621e6
Leilani <pquaby@gmail.com>
2024-05-21 17:52:25 -0400

    Add homeroute

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,13 @@ import React from 'react';
 import './App.scss';
 import PhotoList from 'components/PhotoList';
 import TopNavigation from 'components/TopNavigationBar'
+import HomeRoute from 'routes/HomeRoute';
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   return (
     <div className="App">
-      <TopNavigation />
-      <PhotoList />
+      <HomeRoute />
     </div>
 
   );
