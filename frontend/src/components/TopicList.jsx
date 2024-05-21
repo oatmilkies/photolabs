c8d06da97cd7a8e9a083f373d6c6e9336e59027b
Leilani <pquaby@gmail.com>
2024-05-21 17:57:12 -0400

    Refactor to use mock data in photo list and topic list

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -1,28 +1,12 @@
 import React from "react";
 
 import "../styles/TopicList.scss";
+import topics from "mocks/topics";
 import TopicListItem from "./TopicListItem";
 
-const sampleDataForTopicList = [
-  {
-    id: "1",
-    slug: "topic-1",
-    title: "Nature",
-  },
-  {
-    id: "2",
-    slug: "topic-2",
-    title: "Travel",
-  },
-  {
-    id: "3",
-    slug: "topic-3",
-    title: "People",
-  },
-];
-
+//Display list of topics
 const TopicList = () => {
-  const topicItems = sampleDataForTopicList.map((item) =>
+  const topicItems = topics.map((item) =>
     <TopicListItem key={item.id} listItem={item} />
   );
 
