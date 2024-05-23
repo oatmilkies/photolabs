93b911cf7525f303644d3376382d1c58e605763a
Leilani <pquaby@gmail.com>
2024-05-23 18:31:26 -0400

    More refactoring for modal photo data

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -4,7 +4,8 @@ import '../styles/PhotoDetailsModal.scss'
 import closeSymbol from '../assets/closeSymbol.svg';
 
 const PhotoDetailsModal = (props) => {
-
+  console.log(props.selectedPhoto)
+  
   return (
     <div className="photo-details-modal">
       <button className="photo-details-modal__close-button" onClick={props.toggleModal} >
