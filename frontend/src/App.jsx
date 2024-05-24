c01b4627e6ef7006f21f45bd77408b19ca902e80
Leilani <pquaby@gmail.com>
2024-05-24 15:35:48 -0400

    Remove unused code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import './App.scss';
 import HomeRoute from 'routes/HomeRoute';
@@ -9,36 +9,6 @@ import useApplicationData from 'hooks/useApplicationData';
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
-  // //Get data for the selected photo to pass to modal
-  // const [displayModal, setDisplayModal] = useState(false);
-  // const [selectedPhoto, setSelectedPhoto] = useState({});
-
-  // const toggleModal = (photoID) => {
-  //   setDisplayModal(prevState => !prevState);
-
-  //   let updatedSelectedPhoto = {};    
-
-  //   for (const photo of photos) {
-  //     if (photo.id === photoID) {
-  //       updatedSelectedPhoto = {photo}
-  //       setSelectedPhoto(updatedSelectedPhoto);
-  //     }
-  //   }
-  // };
-
-  // //Check if photo is in liked list. Remove if it is, add if it's not
-  // const [likedPhotos, setLikedPhotos] = useState([]);
-
-  // const toggleLike = function(photoId) {
-  //   if (likedPhotos.includes(photoId)) {
-  //     const updatedLikedPhotos = likedPhotos.filter(id => id !== photoId);
-  //     setLikedPhotos(updatedLikedPhotos);
-  //   } else {
-  //     const updatedLikedPhotos = [...likedPhotos, photoId];
-  //     setLikedPhotos(updatedLikedPhotos);
-  //   }
-  // };
-
 
   const {
     displayModal,
