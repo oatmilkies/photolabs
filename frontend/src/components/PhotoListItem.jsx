a25db648a686480325d4bd8d14d999203f6e5844
Leilani <pquaby@gmail.com>
2024-05-25 08:50:18 -0400

    Adjust formatting

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -6,23 +6,26 @@ import PhotoFavButton from "./PhotoFavButton";
 //Display a single photo and user details
 const PhotoListItem = (props) => {
   const { liked, toggleFav } = props;
-  
+
   return (
-    <li className="photo-list"  key={props.listItem.id}>
-    <div className="photo-list__item">
-      <PhotoFavButton liked={liked} toggleFav={toggleFav}/>
-      <img className="photo-list__image" src={props.listItem.urls.regular} onClick={() => {props.toggleModal(); props.selectPhoto(props.listItem.id);}}></img>
-      <div className="photo-list__user-details">
-      <img className="photo-list__user-profile" src={props.listItem.user.profile}></img>
-        <div className="photo-list__user-info">
-          <span>{props.listItem.user.name}</span>
-          <div className="photo-list__user-location">
-            <span>{props.listItem.location.city} </span>
-            <span>{props.listItem.location.country}</span>
+    <li className="photo-list" key={props.listItem.id}>
+      <div className="photo-list__item">
+
+        <PhotoFavButton liked={liked} toggleFav={toggleFav} />
+        <img className="photo-list__image" src={props.listItem.urls.regular} onClick={() => { props.toggleModal(); props.selectPhoto(props.listItem.id); }}></img>
+
+        <div className="photo-list__user-details">
+          <img className="photo-list__user-profile" src={props.listItem.user.profile}></img>
+          <div className="photo-list__user-info">
+            <span>{props.listItem.user.name}</span>
+            <div className="photo-list__user-location">
+              <span>{props.listItem.location.city} </span>
+              <span>{props.listItem.location.country}</span>
+            </div>
           </div>
         </div>
+        
       </div>
-    </div>
     </li>
   );
 };
